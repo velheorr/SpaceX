dc8a2b26c9a005e0e0dfb1708cfc92df278d6a75
Iurii <velheorr@gmail.com>
2021-12-14 16:57:20 +0500

    сlear logs

--- src/api/api.js
+++ src/api/api.js
@@ -43,12 +43,3 @@ export const api = {
     }
 
 }
-
-//https://api.spacexdata.com/v5/launches/     .name = name, .rocket = id
-// https://api.spacexdata.com/v4/rockets/ +id  .name  = Falcon 1
-
-
-//  https://api.spacexdata.com/v4/launches/upcoming  upcoming
-// .rocket = id
-// .name
-
